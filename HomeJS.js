fbd303a1c16239fb210896757ab6afaa210ee6be
HabibaMoharam <habibamoharam24@gmail.com>
2024-12-18 15:54:03 +0200

    Initial commit

---
+++ HomeJS.js
@@ -0,0 +1,8 @@
+// Toggle Hamburger Menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
